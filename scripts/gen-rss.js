f3fcf6687378b286e8d73776e9ba3ef357ab90bf
kumarann <kumarann@umich.edu>
2024-02-16 23:24:26 +1100

    updated configs

--- scripts/gen-rss.js
+++ scripts/gen-rss.js
@@ -5,9 +5,9 @@ const matter = require('gray-matter')
 
 async function generate() {
   const feed = new RSS({
-    title: 'Your Name',
-    site_url: 'https://yoursite.com',
-    feed_url: 'https://yoursite.com/feed.xml'
+    title: 'Kumaran Nathan',
+    site_url: 'https://kumarannathan  .com',
+    feed_url: 'https://kumarannathan.com/feed.xml'
   })
 
   const posts = await fs.readdir(path.join(__dirname, '..', 'pages', 'posts'))
